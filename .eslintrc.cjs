b9309965afdb76b80e9da9187efa8aaa4201c77b
tcaicai <1014934017@qq.com>
2022-10-17 22:07:35 +0800

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+    ],
+    "overrides": [
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "parser": "@typescript-eslint/parser"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        // 解决 Don‘t use `{}` as a type. `{}` actually means “any non-nullish value“
+        "@typescript-eslint/ban-types": [
+            "error",
+            {
+                "extendDefaults": true,
+                "types": {
+                    "{}": false
+                }
+            }
+        ]
+    }
+}
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import logo from '@/assets/vue.svg';
+</script>
+
+<template>
+  <div class="home" >v3</div>
+  <img :src="logo" />
+</template>
+
+<style scoped>
+.home {
+  color: pink;
+}
+</style>
