4822dcb7a001fbe2ddd637c84585533ecd0c76ff
tcaicai <1014934017@qq.com>
2022-10-27 11:36:32 +0800

    feat: eslint配置完善

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
-const root = "src/pages/";
+const root = 'src/pages/';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
     root,
-    base: "",
+    base: '',
     resolve: {
       alias: {
-        "@": resolve(__dirname, "./src"),
+        '@': resolve(__dirname, './src'),
       },
     },
     plugins: [vue()],
     build: {
       rollupOptions: {
         input: {
-          spring: resolve(__dirname, "src/pages/spring/index.html"),
-          summer: resolve(__dirname, "src/pages/summer/index.html"),
-          autumn: resolve(__dirname, "src/pages/autumn/index.html"),
-          winter: resolve(__dirname, "src/pages/winter/index.html"),
+          spring: resolve(__dirname, 'src/pages/spring/index.html'),
+          summer: resolve(__dirname, 'src/pages/summer/index.html'),
+          autumn: resolve(__dirname, 'src/pages/autumn/index.html'),
+          winter: resolve(__dirname, 'src/pages/winter/index.html'),
         },
         output: {
-          dir: "./dist",
+          dir: './dist',
         },
       },
     },
